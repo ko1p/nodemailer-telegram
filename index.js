644c2a740124e02aca6c5d4d80800f7e4ef090cb
Anton Belko <30528165+ko1p@users.noreply.github.com>
2021-03-29 17:25:36 +0300

    change route

--- index.js
+++ index.js
@@ -4,23 +4,19 @@ const mailer = require('./nodemailer')
 
 const app = express()
 
-const PORT = 80
-let user = undefined
-
-// app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'))
+const PORT = process.env.PORT || 80
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-// app.use(bodyParser.urlencoded({ extended: false }))
-app.post('/registration', (req, res) => { 
+app.post('/send', (req, res) => { 
     console.log(req.body)
     if(!req.body.name || !req.body.phone) return res.sendStatus(400)   
     const message = {        
         to: 'ko1p@ya.ru',
         subject: 'Новая заявка',
         html: `
-        <h2>Пришла новая заявка</h2>
+        <h2>Новая заявка</h2>
         
         <i>Контактная информация клиента:</i>
         <ul>
@@ -32,18 +28,7 @@ app.post('/registration', (req, res) => {
     }
 
     res.status(200).send(`Заявка успешно ушла на почту ${message.to}`)
-    mailer(message) 
-    // user = req.body 
-    // res.redirect('/registration') 
+    mailer(message)
 })
-// app.get('/registration', (req, res) => { 
-//     if(typeof user !== 'object') return res.sendFile(__dirname + '/registration.html')   
-//     res.send(`Регистрация прошла успешно! Данные учетной записи отправлены на email: ${user.email}`) 
-//     user = undefined  
-// })
-// app.get('/unsubscribe/:email', (req, res) => {
-//     console.log(`${req.params.email} unsubscribed`)
-//     res.send(`Ваш email: ${req.params.email} удален из списка рассылки!`)
-// })
 
-app.listen(process.env.PORT, () => console.log(`server listening at http://localhost:${process.env.PORT}/registration`))
+app.listen(PORT, () => console.log(`Сервер запустился на ${PORT} порту`))
--- nodemailer.js
+++ nodemailer.js
@@ -1,26 +1,27 @@
-  
+require('dotenv').config();
+const { PASSWORD, EMAIL } = process.env;
+
 const nodemailer = require('nodemailer')
 
 const transporter = nodemailer.createTransport(
     {
         host: 'smtp.gmail.com',
         port: 587,
-        secure: false, // true for 465, false for other ports
-        // Пожалуйста, используйте свой собственный gmail аккаунт для рассылки
+        secure: false,
         auth: {
-            user: 'zaavkan@gmail.com', // (замените звездочики на название вашего почтового ящика gmail) 
-            pass: 'zaavkan2021' // (замените звездочики на название вашего почтового ящика) 
+            user: EMAIL,
+            pass: PASSWORD
         }
     },
     {
-        from: 'На сайте новая заявка <zaavkan@gmail.com>' // (замените звездочики на название вашего почтового ящика gmail) 
+        from: 'На сайте новая заявка <zaavkan@gmail.com>'
     }
 )
 
 const mailer = message => {
     transporter.sendMail(message, (err, info) => {
         if(err) return console.log(err)
-        console.log('Email sent: ', info)
+        console.log('Информация о сообщении: ', info)
     })
 }
 
